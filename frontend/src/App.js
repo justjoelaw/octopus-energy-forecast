16c927bd4093b5d2b2226580f66b149b52fa6e03
Joe Lawson <j.lawson.econ@gmail.com>
2023-01-14 16:43:25 +0000

    add csrf token to POST requests 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,11 +50,12 @@ function App() {
 
   // Get Cookie
   function getCookie(name) {
-    var cookieValue = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
@@ -62,7 +63,7 @@ function App() {
         }
     }
     return cookieValue;
-}
+  }
 
   // Fetch latest plots
   const fetchPlotListLatest = async () => {
