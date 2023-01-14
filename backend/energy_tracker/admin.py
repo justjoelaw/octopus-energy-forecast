c9deb7a85dfc08b7920b9065bf7270c3f1599cde
Joe Lawson <j.lawson.econ@gmail.com>
2023-01-14 16:24:52 +0000

    add admin app

--- backend/energy_tracker/admin.py
+++ backend/energy_tracker/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+admin.site.register(Historical_Gas)
+admin.site.register(Historical_Electric)
+admin.site.register(Historical_Plots)
+admin.site.register(Historical_Weather)
+admin.site.register(Predictions)
