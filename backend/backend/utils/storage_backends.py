82508d0cbb7bb8ebcda38ceb5ae7793aabeb3801
Joe Lawson <j.lawson.econ@gmail.com>
2023-01-17 10:32:42 +0000

    attempt to fix storage_backends

--- backend/backend/utils/storage_backends.py
+++ backend/backend/utils/storage_backends.py
@@ -4,14 +4,14 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     """Used to manage static files for the web server"""
-    location = settings.STATIC_LOCATION
-    default_acl = settings.STATIC_DEFAULT_ACL
+    location = 'static'
+    default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
     """Used to store & serve dynamic media files with no access expiration"""
-    location = settings.PUBLIC_MEDIA_LOCATION
-    default_acl = settings.PUBLIC_MEDIA_DEFAULT_ACL
+    location = 'media/public'
+    default_acl = 'public-read'
     file_overwrite = False
 
 
@@ -20,7 +20,7 @@ class PrivateMediaStorage(S3Boto3Storage):
     Used to store & serve dynamic media files using access keys
 	and short-lived expirations to ensure more privacy control
     """
-    location = settings.PRIVATE_MEDIA_LOCATION
-    default_acl = settings.PRIVATE_MEDIA_DEFAULT_ACL
+    location = 'media/private'
+    default_acl = 'private'
     file_overwrite = False
     custom_domain = False
